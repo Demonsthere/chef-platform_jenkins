9e2503da2f2afad23e325f52cbdd7d515cf7fd82
Jakub Blaszczyk | hybris <jakub.blaszczyk@hybris.com>
2016-05-23 21:17:09 +0200

    moved sfdisk to qemu. Why was that here?

--- recipes/slave.rb
+++ recipes/slave.rb
@@ -9,19 +9,6 @@ cookbook_file '/etc/sudoers.d/jenkins' do
   mode '0440'
 end
 
-execute 'Backup original sfdisk' do
-  command 'mv sfdisk sfdisk.back'
-  cwd '/sbin/'
-  action :run
-end
-
-cookbook_file '/sbin/sfdisk' do
-  owner 'root'
-  group 'root'
-  mode '0755'
-  source 'sfdisk'
-end
-
 service 'jenkins-swarm-slave' do
   action [:enable, :start]
 end
